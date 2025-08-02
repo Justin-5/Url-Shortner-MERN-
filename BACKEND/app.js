bdac5d090700bcaad1bddf6f89587bbbd38cd554
Justin-5 <justinaroza5@gmail.com>
2025-08-02 23:30:07 +0530

    backend build complete,working on frontend

---
+++ BACKEND/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import {nanoid} from "nanoid"
+import dotenv from "dotenv"
+dotenv.config("./.env")
+import connectDB from "./src/config/monogo.config.js"
+import short_url from "./src/routes/short_url.route.js"
+import { redirectFromShortUrl } from "./src/controller/short_url.controller.js";
+import { errorHandler } from "./src/utils/errorHandler.js"
+import cors from "cors"
+
+
+const app =express();
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use("/api/create",short_url)
+app.get("/:id",redirectFromShortUrl)
+
+app.use(errorHandler)
+app.listen(3000,()=>{
+    connectDB()
+    console.log("Server is running on http://localhost:3000");
+})
